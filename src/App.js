7fdb77807cd5540bfe174259dacf62dd0ea5030e
JMck3 <themckeerings@gmail.com>
2021-10-19 19:44:16 +1000

    update all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import HomePage from "./Pages/HomePage";
+import ProjectPage from "./Pages/ProjectPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    <Nav />
+
+    <Switch>
+      <Route exact path ="/">
+      <HomePage />
+      </Route>
+      <Route path="/project">
+      <ProjectPage />
+      </Route>
+    </Switch>
     </div>
+    </Router>
   );
 }
 
