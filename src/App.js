256786b8d47c39ce0e3644595249253547a67e2d
JMck3 <themckeerings@gmail.com>
2021-10-19 20:22:29 +1000

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import HomePage from "./Pages/HomePage";
@@ -8,18 +8,18 @@ import ProjectPage from "./Pages/ProjectPage";
 function App() {
   return (
     <Router>
-    <div>
-    <Nav />
+      <div>
+        <Nav />
 
-    <Switch>
-      <Route exact path ="/">
-      <HomePage />
-      </Route>
-      <Route path="/project">
-      <ProjectPage />
-      </Route>
-    </Switch>
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/project">
+            <ProjectPage />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
