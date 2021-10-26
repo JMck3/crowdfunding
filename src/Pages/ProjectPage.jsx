2e314ea48f30cb9cb5745df2e486a46ba848831a
JMck3 <themckeerings@gmail.com>
2021-10-26 19:45:55 +1000

    updates to date

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -3,36 +3,38 @@ import { useParams } from "react-router-dom";
 import { oneProject } from "../data";
 
 function ProjectPage() {
-    const [projectData, setProjectData] = useState({pledges: [] });
-    const { id } = useParams();
+  const [projectData, setProjectData] = useState({ pledges: [] });
+  const { id } = useParams();
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
-        .then((results) => {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
+      .then((results) => {
         return results.json();
-        })
-        .then((data) => {
+      })
+      .then((data) => {
         setProjectData(data);
-        });
-    }, []);
+      });
+  }, []);
 
-    return (
-        <div>
-        <h2>{projectData.title}</h2>
-        <h3>Created at: {projectData.date_created}</h3>
-        <h3>{`Status: ${projectData.is_open}`}</h3>
-        <h3>Pledges:</h3>
-        <ul>
+  const formattedDate = new Date(projectData?.date_created).toDateString();
+
+  return (
+    <div>
+      <h2>{projectData?.title}</h2>
+      <h3>Created at: {formattedDate}</h3>
+      <h3>{`Status: ${projectData.is_open}`}</h3>
+      <h3>Pledges:</h3>
+      <ul>
         {projectData.pledges.map((pledgeData, key) => {
-            return (
-            <li>    
-            {pledgeData.amount} from {pledgeData.supporter}
+          return (
+            <li>
+              {pledgeData.amount} from {pledgeData.supporter}
             </li>
-            );
+          );
         })}
-        </ul>
-        </div>
-    );
+      </ul>
+    </div>
+  );
 }
 
 export default ProjectPage;
