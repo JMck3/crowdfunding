72c88f76f1115686fe56d4891dc7a7fa58acd811
JMck3 <themckeerings@gmail.com>
2021-11-02 21:01:40 +1000

    login update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -113,7 +113,8 @@ const LoginForm = () => {
     if (credentials.username && credentials.password) {
       postData().then((response) => {
         window.localStorage.setItem("token", response.token);
-        history.push("/");
+        window.location = `${window.location.origin}/`;
+        // history.push("/");
       });
     }
   };
