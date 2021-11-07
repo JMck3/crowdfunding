83b13dc2bec5c62e688297935851e2eeae365e25
JMck3 <themckeerings@gmail.com>
2021-11-07 22:27:06 +1000

    extra updates

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import error from "../Images/error.svg";
 
 const ErrorPage = () => {
-  return "This is an error page";
+  return <h1>This is an Error Page</h1>;
+  <div></div>;
+  let image = require("../Images/error.svg");
+  <img src={error} alt="image not found" />;
 };
 
 export default ErrorPage;
---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error() {
+  return (
+    <div class="error">
+    <img src=
+    </div>
+  );
+}
+
+export default Footer;
